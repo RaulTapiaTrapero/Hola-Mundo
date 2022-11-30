9390d84e58ed78e7ab57e857ae8c3a08edc53753
Raúl Tapia Trapero <Raultapiatrapero@gmail.com>
2022-11-30 10:08:03 +0100

    Cambiado el mensaje de bienvenida

--- src/main/java/com/example/hola_mundo/hola_mundo.java
+++ src/main/java/com/example/hola_mundo/hola_mundo.java
@@ -2,6 +2,6 @@ package com.example.hola_mundo;
 
 public class hola_mundo {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo Oretano");
     }
 }
